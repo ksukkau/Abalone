d0a1635087ca464d67cadc2c3633f347403b9678
Kat Sukkau <Katsukkau@gmail.com>
2022-03-06 12:05:42 -0800

    correct direction tuples

--- state_space_generator.py
+++ state_space_generator.py
@@ -110,11 +110,11 @@ class StateSpaceGenerator:
         piece = self.translate_piece_value_for_output(int(row_key.replace("row", '')), column_detail["colNum"])
         print(piece)
         move_directions = {
-            "NE": (1, -1),
-            "E": (1, 0),
+            "NE": (-1, 1),
+            "E": (0, 1),
             "SE": (1, 1),
-            "SW": (-1, 1),
-            "W": (-1, 0),
+            "SW": (1, -1),
+            "W": (0, -1),
             "NW": (-1, -1)
         }
         for direction in move_directions:
