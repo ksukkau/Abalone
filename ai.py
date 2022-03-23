f7d5162e5e7fa2410bc7c38e8c8127400529103e
Kat Sukkau <Katsukkau@gmail.com>
2022-03-22 21:52:03 -0700

    creating functions to check proximity to center and working on heuristics added minimax class skeleton

--- ai.py
+++ ai.py
@@ -4,18 +4,36 @@ from game import *
 # TODO transposition table
 # TODO heuristics
 
-####################################### KATS HEURISTIC ##########################################
-def heuristic(board):
-    center_proximity = board.query.center_proximity(False) - board.query.center_proximity(True)
 
-    # cohesion
-    grouping = 0
-    if abs(center_proximity) > 2:
-        cohesion = len(list(board.query.populations(False))) - len(list(board.query.populations(True)))
+# depth limited minimax search
+class Minimax:
 
-    # number of marbles
-    marbles = 0
-    if abs(center_proximity) < 1.8:
-        marbles = board.query.marbles(True, True) * 100 - board.query.marbles(False, True) * 100
+    def __init__(self, state):
+        self.state = state
 
-    return center_proximity + cohesion + marbles
+    def minimax_decision(self):
+        pass
+
+    def max_value(self):
+        pass
+
+    def min_value(self):
+        pass
+
+
+
+# ####################################### KATS HEURISTIC ##########################################
+# def heuristic(board):
+#     center_proximity = board.query.center_proximity(False) - board.query.center_proximity(True)
+#
+#     # cohesion
+#     grouping = 0
+#     if abs(center_proximity) > 2:
+#         grouping = len(list(board.query.populations(False))) - len(list(board.query.populations(True)))
+#
+#     # number of marbles
+#     marbles = 0
+#     if abs(center_proximity) < 1.8:
+#         marbles = board.query.marbles(True, True) * 100 - board.query.marbles(False, True) * 100
+#
+#     return center_proximity + grouping + marbles
--- board_query.py
+++ board_query.py
@@ -1,8 +1,12 @@
 from game import *
+from move import *
+from converter import *
+
 # this needs to receive the board given by the state space generator
 
 test_board = {'row0': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row1': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}], 'row2': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 6, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}], 'row3': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 6, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 7, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row4': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 6, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 7, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 8, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row5': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 6, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 7, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row6': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': 'white', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': 'black', 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 6, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row7': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 5, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}], 'row8': [{'colNum': 0, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 1, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 2, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 3, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}, {'colNum': 4, 'color': None, 'selected': False, 'x_pos': None, 'y_pos': None}]}
 
+move = Move()
 
 def center(board, color):
     """
@@ -15,7 +19,7 @@ def center(board, color):
     pieces = 0
     # 9 rows 9 columns center is E5 or board notation row 4 column 4
     for row in board.items():
-        row_number = row[0].replace('row', '')
+        row_number = Converter.convert_row_to_string_or_int(row[0])
         row = row[1]
         for place in row:
             if place['color'] == color:
@@ -48,18 +52,41 @@ def groups(board, color):
     group_count = 1
     list_of_pieces = []
     for row in board.items():
-        row_number = int(row[0].replace('row', ''))
+        row_number = Converter.convert_row_to_string_or_int(row[0])
         row = row[1]
         for place in row:
             if place['color'] == color:
+                spaces = move.get_adj_game_spaces(row_number, place['colNum'])
+                agjs = space_translation(board, spaces, color)
+                #print(spaces)
                 list_of_pieces.append((row_number, place['colNum']))
-    print(list_of_pieces)
+    #print(list_of_pieces)
     for i in range(len(list_of_pieces)-1):
         row = abs(list_of_pieces[i][0] - list_of_pieces[i+1][0])
         col = abs(list_of_pieces[i][1] - list_of_pieces[i+1][1])
         if row > 1 and col > 1:
             group_count += 1
+
     return group_count
+
+
+def space_translation(board, adj_list, color):
+    """
+    finds any same color adjacent pieces to the current piece
+    :param board:
+    :param adj_list:
+    :param color:
+    :return:
+    """
+    adjs = []
+    for item in adj_list:
+        spot = Converter.external_notation_to_internal(item)
+        for item in board[spot[0]]:
+            if item['colNum'] == spot[1]:
+                if item['color'] == color:
+                    adjs.append(spot)
+
+    print(adjs)
 
 
 #print(groups(test_board, "black"))
